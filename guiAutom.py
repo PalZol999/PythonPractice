8744a32ce541681a53f35540a9b86c732a98d9f1
PalZol <p.z.palasti@gmail.com>
2023-12-15 14:23:22 +0100

    python practice

--- guiAutom.py
+++ guiAutom.py
@@ -1,5 +1,11 @@
 import pyautogui
 
-print(pyautogui.size())
 print(pyautogui.position())
-pyautogui.moveTo(10,10, duration=1.5)
+pyautogui.click(1072,129)
+pyautogui.typewrite(['H', 'enter', 'a', 'b', 'left', 'right', 'X', 'Y'], interval=0.1)
+             
+#pyautogui.locateCentertOnScreen('c:\\image4nim.png')
+
+#pyautogui.moveTo(463,24, duration=1.5)<
+
+#pyautogui.moveRel(0, 100, duration=1.5)
